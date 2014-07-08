798ea47c1f793ee9ace62a59f3c1b138b16158dd
Corey Frenette <corey@narwhunderful.com>
2014-07-07 23:56:17 -0300

    Remove old debug code

--- flamework/src/flamework/http/netty/Handler.java
+++ flamework/src/flamework/http/netty/Handler.java
@@ -1,7 +1,5 @@
 package flamework.http.netty;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import flamework.http.ServerInterface;
@@ -35,7 +33,6 @@ public class Handler extends SimpleChannelInboundHandler<HttpObject> {
   private ServerInterface.Events _events;
   
   private HttpRequest   request;
-  private StringBuilder buf = new StringBuilder();
   private StringBuilder _content = new StringBuilder();
   
   Handler(ServerInterface.Events events) {
@@ -54,38 +51,7 @@ public class Handler extends SimpleChannelInboundHandler<HttpObject> {
         send100Continue(ctx);
       }
       
-      buf.setLength(0);
-      buf.append("WELCOME TO THE WILD WILD WEB SERVER\r\n");
-      buf.append("===================================\r\n");
-      
-      buf.append("VERSION: ").append(request.protocolVersion()).append("\r\n");
-      buf.append("HOSTNAME: ").append(HttpHeaders.getHost(request, "unknown")).append("\r\n");
-      buf.append("REQUEST_URI: ").append(request.uri()).append("\r\n\r\n");
-      
-      HttpHeaders headers = request.headers();
-      if(!headers.isEmpty()) {
-        for(Map.Entry<String, String> h : headers) {
-          String key = h.getKey();
-          String value = h.getValue();
-          buf.append("HEADER: ").append(key).append(" = ").append(value).append("\r\n");
-        }
-        buf.append("\r\n");
-      }
-      
-      QueryStringDecoder queryStringDecoder = new QueryStringDecoder(request.uri());
-      Map<String, List<String>> params = queryStringDecoder.parameters();
-      if(!params.isEmpty()) {
-        for(Map.Entry<String, List<String>> p : params.entrySet()) {
-          String key = p.getKey();
-          List<String> vals = p.getValue();
-          for(String val : vals) {
-            buf.append("PARAM: ").append(key).append(" = ").append(val).append("\r\n");
-          }
-        }
-        buf.append("\r\n");
-      }
-      
-      appendDecoderResult(buf, request);
+      appendDecoderResult(request);
     }
     
     if(msg instanceof HttpContent) {
@@ -94,26 +60,11 @@ public class Handler extends SimpleChannelInboundHandler<HttpObject> {
       ByteBuf content = httpContent.content();
       if(content.isReadable()) {
         _content.append(content.toString(CharsetUtil.UTF_8));
-        buf.append("CONTENT: ");
-        buf.append(content.toString(CharsetUtil.UTF_8));
-        buf.append("\r\n");
-        appendDecoderResult(buf, request);
+        appendDecoderResult(request);
       }
       
       if(msg instanceof LastHttpContent) {
-        buf.append("END OF CONTENT\r\n");
-        
         LastHttpContent trailer = (LastHttpContent)msg;
-        if(!trailer.trailingHeaders().isEmpty()) {
-          buf.append("\r\n");
-          for(String name : trailer.trailingHeaders().names()) {
-            for(String value : trailer.trailingHeaders().getAll(name)) {
-              buf.append("TRAILING HEADER: ");
-              buf.append(name).append(" = ").append(value).append("\r\n");
-            }
-          }
-          buf.append("\r\n");
-        }
         
         _events.raiseRequest(new Request(request, new QueryStringDecoder(request.uri()).parameters(), _content.toString()));
         
@@ -131,22 +82,24 @@ public class Handler extends SimpleChannelInboundHandler<HttpObject> {
     ctx.close();
   }
   
-  private static void appendDecoderResult(StringBuilder buf, HttpObject o) {
+  private static void appendDecoderResult(HttpObject o) {
     DecoderResult result = o.decoderResult();
     if(result.isSuccess()) {
       return;
     }
     
-    buf.append(".. WITH DECODER FAILURE: ");
-    buf.append(result.cause());
-    buf.append("\r\n");
+    //TODO: DecoderError
+    
+    //buf.append(".. WITH DECODER FAILURE: ");
+    //buf.append(result.cause());
+    //buf.append("\r\n");
   }
   
   private boolean writeResponse(HttpObject currentObj, ChannelHandlerContext ctx) {
     // Decide whether to close the connection or not.
     boolean keepAlive = HttpHeaders.isKeepAlive(request);
     // Build the response object.
-    FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, currentObj.decoderResult().isSuccess() ? HttpResponseStatus.OK : HttpResponseStatus.BAD_REQUEST, Unpooled.copiedBuffer(buf.toString(), CharsetUtil.UTF_8));
+    FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, currentObj.decoderResult().isSuccess() ? HttpResponseStatus.OK : HttpResponseStatus.BAD_REQUEST, Unpooled.copiedBuffer("Test", CharsetUtil.UTF_8));
     
     response.headers().set(CONTENT_TYPE, "text/plain; charset=UTF-8");
     
